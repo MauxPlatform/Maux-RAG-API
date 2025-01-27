956cac656f3ae26aae9696b8228433c9ef1b0adf
MisaghMomeniB <jenabemisagh@gmail.com>
2025-01-27 23:07:12 +0330

    Def AvalaiService

--- app/services/avalai_service.py
+++ app/services/avalai_service.py
@@ -1,3 +1,32 @@
 import requests
 from app.config.settings import settings
 
+class AvalaiService:
+    def __init__(self):
+        self.base_url = settings.AVALAI_BASE_URL
+        self.defualt_embedding_model = "text-embedding"
+        self.defualt_chat_model = "chat-completion"
+
+    def create_embedding(self, text:str):
+        response = requests.post(
+            f"{self.base_url}/embedding",
+            json={"input": text, "model": self.default_embedding_model}
+        )
+
+    def create_chat_completion(self, messages: list, model: str = None):
+        response = requests.post(
+            f"{self.base_url}/chat/completions",
+            json={"messages": messages, "model": model or self.default_chat_model}
+        )
+        response.raise_for_status()
+        return response.json()
+    
+    def create_chat_completion_stream(self, messages: list, model: str = None):
+        response = requests.post(
+            f"{self.base_url}/chat/completions",
+            json={"messages": messages, "model": model or self.default_chat_model, "stream": True},
+            stream=True
+        )
+        response.raise_for_status()
+        for chunk in response.iter_lines():
+            yield chunk.decode("utf-8")
